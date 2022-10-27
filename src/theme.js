2e6b7062cf156c90ac6fac20f7cba37a043bd9c4
Hamza405 <hamzash863@gmail.com>
2022-10-27 04:31:38 -0400

    init app

---
+++ src/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#17605a",
+      light: "skyblue",
+    },
+    secondary: {
+      main: "#15c630",
+    },
+    otherColor: {
+      main: "#999",
+    },
+  },
+});
+
+export default theme;
