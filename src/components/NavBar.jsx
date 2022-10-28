690b6ef2169d03936f1a32239ebd8a7df8ba4315
Hamza405 <hamzash863@gmail.com>
2022-10-28 11:23:08 +0200

    appbar menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { AppBar, InputBase, Typography, Badge, Avatar } from "@mui/material";
+import {
+  AppBar,
+  InputBase,
+  Typography,
+  Badge,
+  Avatar,
+  Menu,
+  MenuItem,
+} from "@mui/material";
 import {
   SearchBoxStyle,
   ToolBarStyle,
@@ -7,8 +15,11 @@ import {
   MobileActionsStyle,
 } from "../styles/NavBatStyles";
 import { Android, Mail, Notifications } from "@mui/icons-material";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+  const openMenuHandler = () => setOpenMenu(true);
   return (
     <AppBar position="sticky">
       <ToolBarStyle>
@@ -27,11 +38,12 @@ const NavBar = () => {
             <Notifications />
           </Badge>
           <Avatar
+            onClick={openMenuHandler}
             sx={{ width: 30, height: 30 }}
             src="https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584__340.png"
           />
         </ActionsStyle>
-        <MobileActionsStyle>
+        <MobileActionsStyle onClick={openMenuHandler}>
           <Avatar
             sx={{ width: 30, height: 30 }}
             src="https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584__340.png"
@@ -39,6 +51,24 @@ const NavBar = () => {
           <Typography variant="h6">Hamza</Typography>
         </MobileActionsStyle>
       </ToolBarStyle>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={openMenu}
+        onClose={() => setOpenMenu(false)}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
     </AppBar>
   );
 };
