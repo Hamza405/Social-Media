d14d13a7f5ec78d70fdc35e4ec4d4511d204b2b9
Hamza405 <hamzash863@gmail.com>
2022-10-28 17:51:58 +0200

    sticky sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -21,22 +21,24 @@ import MenuListItem from "./Widgets/MenuListItem";
 const SideBar = () => {
   return (
     <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
-      <List>
-        <MenuListItem text="Home" link="#home" icon={<Home />} />
-        <MenuListItem text="Pages" link="#pages" icon={<Pages />} />
-        <MenuListItem text="Groups" link="#groups" icon={<Group />} />
-        <MenuListItem text="Friends" link="#friends" icon={<Person />} />
-        <MenuListItem text="Settings" link="#settings" icon={<Settings />} />
-        <MenuListItem text="Profile" link="#profile" icon={<AccountBox />} />
-        <ListItem disablePadding>
-          <ListItemButton component="a" href="#profile">
-            <ListItemIcon>
-              <ModeNight />
-            </ListItemIcon>
-            <Switch />
-          </ListItemButton>
-        </ListItem>
-      </List>
+      <Box position="fixed">
+        <List>
+          <MenuListItem text="Home" link="#home" icon={<Home />} />
+          <MenuListItem text="Pages" link="#pages" icon={<Pages />} />
+          <MenuListItem text="Groups" link="#groups" icon={<Group />} />
+          <MenuListItem text="Friends" link="#friends" icon={<Person />} />
+          <MenuListItem text="Settings" link="#settings" icon={<Settings />} />
+          <MenuListItem text="Profile" link="#profile" icon={<AccountBox />} />
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#profile">
+              <ListItemIcon>
+                <ModeNight />
+              </ListItemIcon>
+              <Switch />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Box>
     </Box>
   );
 };
