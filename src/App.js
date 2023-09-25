297f48d882e4b975dd972456f968aff3b35caa6f
Hamza405 <hamzash863@gmail.com>
2023-09-25 10:34:59 +0100

    new design

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
-import React, { useState } from "react";
-import { Box, Stack, ThemeProvider } from "@mui/material";
-import NavBar from "./components/NavBar";
-import Feeds from "./components/Feeds";
-import RightBar from "./components/RightBar";
-import SideBar from "./components/SideBar";
-import AddPostModal from "./components/AddPostModal";
-import useMyTheme from "./hooks/useMyTheme";
-import AppDrawer from "./components/AppDrawer";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
 
 function App() {
-  const myTheme = useMyTheme();
-  const [drawer, setDrawer] = useState(false);
-  return (
-    <ThemeProvider theme={myTheme.theme}>
-      <Box bgcolor={"background.default"} color={"text.primary"}>
-        <NavBar setDrawer={setDrawer} />
-        <AppDrawer drawer={drawer} setDrawer={setDrawer} myTheme={myTheme} />
-        <Stack direction="row" justifyContent="space-between">
-          <SideBar t={myTheme} />
-          <Feeds />
-          <RightBar />
-        </Stack>
-        <AddPostModal />
-      </Box>
-    </ThemeProvider>
-  );
+  return <Profile/>;
 }
 
 export default App;
